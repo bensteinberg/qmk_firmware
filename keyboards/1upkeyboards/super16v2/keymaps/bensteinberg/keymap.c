2a51c0d7ff2871c13e701646cf2210e457ea4163
Ben Steinberg <bsteinberg@law.harvard.edu>
2023-02-12 21:15:49 -0500

    Try tap and hold

--- keyboards/1upkeyboards/super16v2/keymaps/bensteinberg/keymap.c
+++ keyboards/1upkeyboards/super16v2/keymaps/bensteinberg/keymap.c
@@ -16,35 +16,109 @@
 
 #include QMK_KEYBOARD_H
 
+#define _NUMPAD 0
+#define _ALPHA 1
+#define _MODS 2
+
+// Define a type for as many tap dance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum {
+    QUOT_LAYR, // Our custom tap dance key; add any other tap dance keys to this enum 
+};
+
+// Declare the functions to be used with your tap dance key(s)
+
+// Function associated with all tap dances
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// Functions associated with individual tap dances
+void ql_finished(qk_tap_dance_state_t *state, void *user_data);
+void ql_reset(qk_tap_dance_state_t *state, void *user_data);
+
+
+// Determine the current tap dance state
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (!state->pressed) return TD_DOUBLE_TAP;
+        else return TD_DOUBLE_HOLD;
+    }
+    else return TD_UNKNOWN;
+}
+
+// Initialize tap structure associated with example tap dance key
+static td_tap_t ql_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// Functions that control what our tap dance key does
+void ql_finished(qk_tap_dance_state_t *state, void *user_data) {
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(DF(_NUMPAD));
+            break;
+        case TD_SINGLE_HOLD:
+            layer_on(_MODS);
+            break;
+        case TD_DOUBLE_TAP:
+            tap_code(DF(_ALPHA));
+            break;
+        default:
+            break;
+    }
+}
+
+void ql_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // If the key was held down and now is released then switch off the layer
+    if (ql_tap_state.state == TD_SINGLE_HOLD) {
+        layer_off(_MODS);
+    }
+    ql_tap_state.state = TD_NONE;
+}
+
+// Associate our tap dance key with its functionality
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [QUOT_LAYR] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275)
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ortho_4x4( /* Base */
+  [_NUMPAD] = LAYOUT_ortho_4x4( /* Base */
     KC_P7,    KC_P8,    KC_P9,    KC_PMNS,
     KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
     KC_P1,    KC_P2,    KC_P3,    KC_PSLS,
-    MO(1),    KC_P0,    KC_PDOT,  KC_PENT
+    QUOT_LAYR,    KC_P0,    KC_PDOT,  KC_PENT
   ),
 
-  [1] = LAYOUT_ortho_4x4( /* Fn Layer */
+  [_ALPHA] = LAYOUT_ortho_4x4( /* experiment */
+    KC_A,    KC_B,    KC_C,    KC_D,
+    KC_E,    KC_F,    KC_G,    KC_H,
+    KC_I,    KC_J,    KC_K,    KC_L,
+    QUOT_LAYR,    KC_M,    KC_N,  KC_PENT
+  ),
+
+  [_MODS] = LAYOUT_ortho_4x4( /* Fn Layer */
     RGB_TOG,   RGB_HUI,    RGB_SAI,    RGB_VAI,
     RGB_MOD,   RGB_HUD,    RGB_SAD,    RGB_VAD,
     RGB_SPD,   RGB_SPI,    KC_TRNS,    KC_CALCULATOR,
     KC_TRNS,   KC_TRNS,    KC_TRNS,    QK_BOOT
   ),
-};
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code_delay(KC_VOLU, 10);
-        } else {
-            tap_code_delay(KC_VOLD, 10);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            rgb_matrix_increase_hue();
-        } else {
-            rgb_matrix_decrease_hue();
-        }
-    }
-    return false;
-}
+};
